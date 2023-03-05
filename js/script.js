aa3f1bb036d4806564ef50399b8e4bae228879a7
Daniel Ferraz <danielferrazmm@hotmail.com>
2023-03-04 21:07:25 -0300

    pequenas alteraçoes

--- js/script.js
+++ js/script.js
@@ -1,29 +1,36 @@
-$(document).ready(function(){
-    $('#menu').click(function(){
+$(document).ready(function () {
+    $('#menu').click(function () {
         $(this).toggleClass('fa-times')
         $('header').toggleClass('toggle')
+        $('').toggle
     })
 
-    $(window).on('scroll load',function(){
+
+
+    $(window).on('scroll load', function () {
         $('#menu').removeClass('fa-times')
         $('header').removeClass('toggle')
 
-        if ($(window).scrollTop() > 3000){
+        if ($(window).scrollTop() > 250) {
             $('.top').show()
         } else {
             $('.top').hide()
         }
 
     })
+    $('.user img').on('click', function () {
+        $('.user img').attr('src', '/imgs/profile_AI.jpg')
+    })
+    $('a[href*="#"]').on('click', function (e) {
+        e.preventDefault()
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top,
+        },
+            500,
+            'linear'
+        )
+    })
 
-$('a[href*="#"]').on('click', function(e){
-    e.preventDefault()
-    $('html, body').animate({
-        scrollTop : $($(this).attr('href')).offset().top,
-    },
-    500,
-    'linear'
-    )
-})
+
 
 })
